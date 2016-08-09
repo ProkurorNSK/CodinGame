b1df13e6133ac64e12aef11e86a586f143ea4c35
ProkurorNSK <proka@ngs.ru>
2016-08-09 12:05:27 +0700

    3

---
+++ src/DontPanicEpisode1/DontPanicEpisode1.java
@@ -0,0 +1,36 @@
+package DontPanicEpisode1;
+
+import java.util.*;
+
+public class DontPanicEpisode1 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int nbFloors = in.nextInt(); // number of floors
+        int width = in.nextInt(); // width of the area
+        int nbRounds = in.nextInt(); // maximum number of rounds
+        int exitFloor = in.nextInt(); // floor on which the exit is found
+        int exitPos = in.nextInt(); // position of the exit on its floor
+        int nbTotalClones = in.nextInt(); // number of generated clones
+        int nbAdditionalElevators = in.nextInt(); // ignore (always zero)
+        int nbElevators = in.nextInt(); // number of elevators
+        for (int i = 0; i < nbElevators; i++) {
+            int elevatorFloor = in.nextInt(); // floor on which this elevator is found
+            int elevatorPos = in.nextInt(); // position of the elevator on its floor
+        }
+        int i=0;
+        // game loop
+        while (true) {
+            int cloneFloor = in.nextInt(); // floor of the leading clone
+            int clonePos = in.nextInt(); // position of the leading clone on its floor
+            String direction = in.next(); // direction of the leading clone: LEFT or RIGHT
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            if (i==0) System.out.println("WAIT"); // action: WAIT or BLOCK
+            if (i==1) System.out.println("BLOCK");
+            if (i>1) System.out.println("WAIT");
+            i++;
+        }
+    }
+}
