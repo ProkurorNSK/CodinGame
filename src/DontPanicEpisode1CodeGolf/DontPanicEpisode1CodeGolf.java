2a961415e2af6cb8af946d8325c660a011be8a59
ProkurorNSK <proka@ngs.ru>
2016-08-10 14:17:17 +0700

    Don't Panic - Episode 1 - done

--- src/DontPanicEpisode1CodeGolf/DontPanicEpisode1CodeGolf.java
+++ src/DontPanicEpisode1CodeGolf/DontPanicEpisode1CodeGolf.java
@@ -1,32 +1,31 @@
-package DontPanicEpisode1;
-
+package DontPanicEpisode1CodeGolf;
 import java.util.*;
 
-public class DontPanicEpisode1 {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int nbFloors = in.nextInt(); // number of floors
-        int width = in.nextInt(); // width of the area
-        int nbRounds = in.nextInt(); // maximum number of rounds
-        int exitFloor = in.nextInt(); // floor on which the exit is found
-        int exitPos = in.nextInt(); // position of the exit on its floor
-        int nbTotalClones = in.nextInt(); // number of generated clones
-        int nbAdditionalElevators = in.nextInt(); // ignore (always zero)
-        int nbElevators = in.nextInt(); // number of elevators
+public class DontPanicEpisode1CodeGolf {
+    public static void main(String a[]) {
+        Scanner I = new Scanner(System.in);
+        int nbFloors = I.nextInt(); // number of floors
+        int width = I.nextInt(); // width of the area
+        int nbRounds = I.nextInt(); // maximum number of rounds
+        int exitFloor = I.nextInt(); // floor on which the exit is found
+        int exitPos = I.nextInt(); // position of the exit on its floor
+        int nbTotalClones = I.nextInt(); // number of generated clones
+        int nbAdditionalElevators = I.nextInt(); // ignore (always zero)
+        int nbElevators = I.nextInt(); // number of elevators
         int[] elevators = new int[nbElevators+1];
         elevators[exitFloor] = exitPos;
         for (int i = 0; i < nbElevators; i++) {
-            int elevatorFloor = in.nextInt(); // floor on which this elevator is found
-            int elevatorPos = in.nextInt(); // position of the elevator on its floor
+            int elevatorFloor = I.nextInt(); // floor on which this elevator is found
+            int elevatorPos = I.nextInt(); // position of the elevator on its floor
             elevators[elevatorFloor] = elevatorPos;
         }
         int targetFloor = 0;
         int posNearestTarget = 0;
         // game loop
         while (true) {
-            int cloneFloor = in.nextInt(); // floor of the leading clone
-            int clonePos = in.nextInt(); // position of the leading clone on its floor
-            String direction = in.next(); // direction of the leading clone: LEFT or RIGHT
+            int cloneFloor = I.nextInt(); // floor of the leading clone
+            int clonePos = I.nextInt(); // position of the leading clone on its floor
+            String direction = I.next(); // direction of the leading clone: LEFT or RIGHT
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
