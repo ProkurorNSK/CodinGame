68448749b29dafdc93ffb695a1d983f29f633ea2
ProkurorNSK <proka@ngs.ru>
2016-08-15 00:58:34 +0700

    HeartOfTheCity start

--- src/HeartOfTheCity/HeartOfTheCity.java
+++ src/HeartOfTheCity/HeartOfTheCity.java
@@ -6,21 +6,23 @@ public class HeartOfTheCity {
     public static void main(String args[]) {
 //        Scanner in = new Scanner(System.in);
 //        long n = in.nextLong();
-        long n = 1191;
-        ArrayList<Double> listAngle = new ArrayList<>();
+        long n = 333;
+        int preResult;
         int result = 0;
         int count = 0;
-        Double angle;
 
         for (int i = 1 ; i <= (n-1)/2; i++) {
+            preResult = 0;
             for (int j = 0; j < i; j++) {
-                angle = new Double((double)j/i);
-                if (!listAngle.contains(angle)) {
-                    listAngle.add(angle);
-                    result++;
+                if (gcd(i,j) == 1) {
+//                    result++;
+                    preResult++;
+//                    System.out.println((double)j/i);
                 }
                 count++;
             }
+            result += getResult(i);
+            System.out.println(i + ":" + preResult);
         }
 
         // Write an action using System.out.println()
@@ -29,5 +31,24 @@ public class HeartOfTheCity {
         System.out.println(result*8);
         System.out.println(result);
         System.out.println(count);
+    }
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+    private static int getResult(int n) {
+        int result = (n==1)?1:n-1;
+
+        for (int i = 2; i <= n; i++) {
+            if (n%i == 0) {
+                System.out.print(i + " ");
+                n = n/i;
+            }
+        }
+
+        return result;
     }
 }
