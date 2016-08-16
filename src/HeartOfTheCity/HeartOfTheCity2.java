180e0409dc0ad59aa52cd22b167ae7901b8bf7fe
ProkurorNSK <proka@ngs.ru>
2016-08-16 16:59:44 +0700

    10

--- src/HeartOfTheCity/HeartOfTheCity2.java
+++ src/HeartOfTheCity/HeartOfTheCity2.java
@@ -1,28 +1,28 @@
 package HeartOfTheCity;
 
-import java.util.*;
+public class HeartOfTheCity2 {
 
-public class HeartOfTheCity {
     public static int count;
-    public static void main(String args[]) {
-//        Scanner in = new Scanner(System.in);
-//        long n = in.nextLong();
-        long n = 999999;
-        FareiSequence((int)(n-1)/2);
-        System.out.println((count+1) * 8);
+    public static void main(String[] args) {
+        FareiSequence(7);
+        System.out.println(count);
     }
     public static void FareiSequence(int n)
     {
+        System.out.println(0 + "/" + 1);
         FareiSequence(n, 0, 1, 1, 1);
+        System.out.println( 1 + "/" + 1);
     }
+
     private static void FareiSequence(int n, int x, int y, int z, int t)
     {
         int a = x + z, b = y + t;
         if (b <= n)
         {
             FareiSequence(n, x, y, a, b);
-            FareiSequence(n, a, b, z, t);
+            System.out.println(a + "/" + b);
             count++;
+            FareiSequence(n, a, b, z, t);
         }
     }
 }
