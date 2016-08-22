7a13b700e9e18b4ab70df68c8f4fe30160eca41e
ProkurorNSK <proka@ngs.ru>
2016-08-22 16:16:51 +0700

    10

--- src/HeartOfTheCity/HeartOfTheCity2.java
+++ src/HeartOfTheCity/HeartOfTheCity2.java
@@ -1,28 +1,31 @@
 package HeartOfTheCity;
 
-public class HeartOfTheCity2 {
+//import java.util.*;
 
-    public static int count;
-    public static void main(String[] args) {
-        FareiSequence(333);
-        System.out.println(count);
-    }
-    public static void FareiSequence(int n)
-    {
-        System.out.println(0 + "/" + 1);
-        FareiSequence(n, 0, 1, 1, 1);
-        System.out.println( 1 + "/" + 1);
-    }
-
-    private static void FareiSequence(int n, int x, int y, int z, int t)
-    {
-        int a = x + z, b = y + t;
-        if (b <= n)
-        {
-            FareiSequence(n, x, y, a, b);
-            System.out.println(a + "/" + b);
-            count++;
-            FareiSequence(n, a, b, z, t);
+public class HeartOfTheCity {
+    public static void main(String args[]) {
+//        Scanner in = new Scanner(System.in);
+//        long n = in.nextLong();
+        long n = 999999;
+        long time = System.currentTimeMillis();
+        long result = 0;
+        for (int i = 1 ; i <= (n-1)/2; i++) {
+            result += phi(i);
         }
+        System.out.println(result*8);
+        System.out.println("Время выполнения(мс) - " +  (System.currentTimeMillis() - time));
+    }
+    private static long phi(int n) {
+        long result = n;
+        for (int i=2; i*i<=n; ++i) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                result -= result / i;
+            }
+        }
+        if (n > 1) result -= result / n;
+        return result;
     }
 }
