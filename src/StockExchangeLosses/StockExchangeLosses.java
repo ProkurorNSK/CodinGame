6bf498ef9e1d9b2c0fbb19c3e7a3463781f35f25
ProkurorNSK <proka@ngs.ru>
2016-08-23 02:53:47 +0700

    12 lvl

--- src/StockExchangeLosses/StockExchangeLosses.java
+++ src/StockExchangeLosses/StockExchangeLosses.java
@@ -6,30 +6,19 @@ public class StockExchangeLosses {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int result = Integer.MAX_VALUE;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int iMin = 0;
-        int iMax = 0;
-        int[] array = new int[n];
+        long time = System.currentTimeMillis();
+        int result = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
             int v = in.nextInt();
-            array[i] = v;
-            if (v < min && i >= iMax) {
-                min = v; iMin = i;
-            }
-            if (v > max && i <= iMin){
-                max = v; iMax = i;
-            }
-            System.out.println(max + " " + min + " - " + iMax + " " + iMin);
+            result = Math.min(result, v - max);
+            max = Math.max(max,v);
         }
 
-        int[] tempArray = Arrays.copyOf(array,array.length);
-
-        result = min - max;
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
         System.out.println(result < 0 ? result : 0);
+        System.out.println("Время выполнения(мс) - " + (System.currentTimeMillis() - time));
     }
 }
