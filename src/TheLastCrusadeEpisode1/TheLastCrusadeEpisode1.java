0fdb9f9823f3f429d79c63e4ccfa1652002aa6c4
ProkurorNSK <proka@ngs.ru>
2016-08-23 03:30:45 +0700

    12 lvl

--- src/TheLastCrusadeEpisode1/TheLastCrusadeEpisode1.java
+++ src/TheLastCrusadeEpisode1/TheLastCrusadeEpisode1.java
@@ -8,10 +8,22 @@ public class TheLastCrusadeEpisode1 {
         int W = in.nextInt(); // number of columns.
         int H = in.nextInt(); // number of rows.
         in.nextLine();
+        int[][] field = new int[H][W];
         for (int i = 0; i < H; i++) {
             String LINE = in.nextLine(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
+            String[] lines = LINE.split(" ");
+            for (int j = 0; j < W; j++) {
+                field[i][j] = Character.digit(lines[j].toCharArray()[0], 10);
+            }
         }
         int EX = in.nextInt(); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                System.err.print(field[i][j] + " ");
+            }
+            System.err.println("");
+        }
 
         // game loop
         while (true) {
