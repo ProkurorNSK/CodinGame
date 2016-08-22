165a9d8995fc26f90b155cdda21d8e7bd48343d9
ProkurorNSK <proka@ngs.ru>
2016-08-23 01:46:31 +0700

    Home1

--- src/HeartOfTheCity/HeartOfTheCity2.java
+++ src/HeartOfTheCity/HeartOfTheCity2.java
@@ -2,7 +2,7 @@ package HeartOfTheCity;
 
 //import java.util.*;
 
-public class HeartOfTheCity {
+public class HeartOfTheCity2 {
     public static void main(String args[]) {
 //        Scanner in = new Scanner(System.in);
 //        long n = in.nextLong();
