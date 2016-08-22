1fe89cae14964f249ab5c0ff256d43f7ac80304c
ProkurorNSK <proka@ngs.ru>
2016-08-23 01:35:58 +0700

    Home

--- src/HeartOfTheCity/HeartOfTheCity.java
+++ src/HeartOfTheCity/HeartOfTheCity.java
@@ -1,55 +1,31 @@
 package HeartOfTheCity;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class HeartOfTheCity {
     public static int count;
     public static void main(String args[]) {
-//        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 //        long n = in.nextLong();
-        long n = 333;
-        int preResult;
-        int result = 0;
-        int count = 0;
-
+        long n = 9999999;
+        long time = System.currentTimeMillis();
+        long result = 0;
         for (int i = 1 ; i <= (n-1)/2; i++) {
-            preResult = 0;
-            for (int j = 0; j < i; j++) {
-                if (gcd(i,j) == 1) {
-                    result++;
-                    preResult++;
-//                    System.out.println((double)j/i);
-                }
-                count++;
-            }
-//            result += getResult(i);
-            System.out.println(i + ":" + preResult);
+            result += phi(i);
         }
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
         System.out.println(result*8);
-        System.out.println(result);
-        System.out.println(count);
+        System.out.println("Время исполнения в мс - " + (System.currentTimeMillis() - time));
     }
-    private static int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        } else {
-            return gcd(b, a % b);
-        }
-    }
-    private static int getResult(int n) {
-        int result = (n==1)?1:n-1;
-
-        for (int i = 2; i <= n; i++) {
-            if (n%i == 0) {
-//                System.out.print(i + " ");
-                n = n/i;
+    private static long phi(int n) {
+        long result = n;
+        for (int i=2; i*i<=n; ++i)
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                result -= result / i;
             }
-        }
-
+        if (n > 1) result -= result / n;
         return result;
     }
 
