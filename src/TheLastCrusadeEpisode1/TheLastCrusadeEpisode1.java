017cac780794f651c3672a3ac1ad3eff246b78f1
ProkurorNSK <proka@ngs.ru>
2016-08-23 14:14:57 +0700

    lvl 12

--- src/TheLastCrusadeEpisode1/TheLastCrusadeEpisode1.java
+++ src/TheLastCrusadeEpisode1/TheLastCrusadeEpisode1.java
@@ -8,35 +8,56 @@ public class TheLastCrusadeEpisode1 {
         int W = in.nextInt(); // number of columns.
         int H = in.nextInt(); // number of rows.
         in.nextLine();
+
         int[][] field = new int[H][W];
+        int dX = 0;
+        int dY = 0;
+
         for (int i = 0; i < H; i++) {
             String LINE = in.nextLine(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
-            String[] lines = LINE.split(" ");
+            String[] numbers = LINE.split(" ");
             for (int j = 0; j < W; j++) {
-                field[i][j] = Character.digit(lines[j].toCharArray()[0], 10);
+                field[i][j] = Integer.parseInt(numbers[j]);
             }
         }
         int EX = in.nextInt(); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
-
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                System.err.print(field[i][j] + " ");
-            }
-            System.err.println("");
-        }
 
         // game loop
         while (true) {
             int XI = in.nextInt();
             int YI = in.nextInt();
             String POS = in.next();
-
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
-
-            // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
-            System.out.println("0 0");
+            int element = field[YI][XI];
+            switch (element) {
+                case 1: case 3: case 7: case 8: case 9: case 12: case 13:
+                    dX = 0; dY = 1; break;
+                case 2:case 6:
+                    if (POS.equals("LEFT")) {
+                        dX = 1; dY = 0;
+                    } else {
+                        dX = -1; dY = 0;
+                    }
+                    break;
+                case 4:
+                    if (POS.equals("RIGHT")) {
+                        dX = 0; dY = 1;
+                    } else {
+                        dX = -1; dY = 0;
+                    }
+                    break;
+                case 5:
+                    if (POS.equals("LEFT")) {
+                        dX = 0; dY = 1;
+                    } else {
+                        dX = 1; dY = 0;
+                    }
+                    break;
+                case 10:
+                    dX = -1; dY = 0; break;
+                case 11:
+                    dX = 1; dY = 0; break;
+            }
+            System.out.println((XI + dX) + " " + (YI + dY));
         }
     }
 }
