957e6544fe7356f81114f26412ca4ab6959aab96
ProkurorNSK <proka@ngs.ru>
2016-09-08 09:35:17 +0700

    lvl 14

---
+++ src/PuzzleCommunity/HillNotationAndHillOrder/HillNotationAndHillOrder.java
@@ -0,0 +1,18 @@
+package PuzzleCommunity.HillNotationAndHillOrder;
+
+import java.util.*;
+
+public class HillNotationAndHillOrder {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int numCompounds = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < numCompounds; i++) {
+            String compound = in.nextLine();
+        }
+
+
+
+        System.out.println("answer");
+    }
+}
