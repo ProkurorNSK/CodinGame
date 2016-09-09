ced0f7f2ecb1562e97689963e88ab6ae27f0f361
ProkurorNSK <proka@ngs.ru>
2016-09-09 11:44:09 +0700

    lvl 14

--- src/PuzzleCommunity/HillNotationAndHillOrder/HillNotationAndHillOrder.java
+++ src/PuzzleCommunity/HillNotationAndHillOrder/HillNotationAndHillOrder.java
@@ -7,19 +7,33 @@ public class HillNotationAndHillOrder {
         Scanner in = new Scanner(System.in);
         int numCompounds = in.nextInt();
         in.nextLine();
-        ArrayList<> formuls;
+        ArrayList<HashMap<String, Integer>> formuls = new ArrayList<>();
         HashMap<String, Integer> formula;
         String buffer = "";
+        String bufferNumber = "";
         for (int i = 0; i < numCompounds; i++) {
             char[] compound = in.nextLine().toCharArray();
-            for (char symbol: compound) {
+            formula = new HashMap<>();
+            for (char symbol : compound) {
                 if (Character.isUpperCase(symbol)) {
+                    if (!buffer.isEmpty()) {
+                        formula.put(buffer, bufferNumber.isEmpty() ? 1 : Integer.parseInt(bufferNumber));
+                        buffer = "";
+                        bufferNumber = "";
+                    }
                     buffer += symbol;
-                } else
+                } else if (Character.isLowerCase(symbol)) {
+                    buffer += symbol;
+                } else if (Character.isDigit(symbol)) {
+                    bufferNumber += symbol;
+                }
             }
-
+            formula.put(buffer, bufferNumber.isEmpty() ? 1 : Integer.parseInt(bufferNumber));
+            buffer = "";
+            bufferNumber = "";
+            formuls.add(formula);
         }
 
-        System.out.println("answer");
+        System.out.println(formuls);
     }
 }
