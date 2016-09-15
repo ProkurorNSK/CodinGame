a3b7a2ef8b1eff4540590bc1a4d37c0e29d9bc70
ProkurorNSK <proka@ngs.ru>
2016-09-15 22:08:46 +0700

    to 13 lvl

--- src/PuzzleCommunity/DivideTheFactorial/DivideTheFactorial.java
+++ src/PuzzleCommunity/DivideTheFactorial/DivideTheFactorial.java
@@ -7,12 +7,10 @@ public class DivideTheFactorial {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
         int B = in.nextInt();
-
-        int last;
         int count = 0;
-        for (int i = 2; i * i <= A; ++i) {
+
+        for (int i = 2; i * i <= A; ++i, count = 0) {
             if (A % i == 0) {
-                count = 0;
                 while (A != i && A % i == 0) {
                     A /= i;
                     count++;
