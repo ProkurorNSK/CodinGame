b20fc1bc83f7189f0b0709ff56790f8f536579fa
ProkurorNSK <proka@ngs.ru>
2016-09-20 02:18:19 +0700

    to 14 lvl

--- src/Tests/test.java
+++ src/Tests/test.java
@@ -1,10 +1,20 @@
 package Tests;
 
 
+import java.util.Properties;
+
 public class test {
     public static void main(String[] args) {
         Integer a = new Integer(1);
+        int s = a.hashCode();
         Class<? extends Number> c = a.getClass();
         System.out.println(c);
+
+        Properties v = System.getProperties();
+
+        System.out.println(a.getClass().getName() + '@' + Integer.toHexString(a.hashCode()));
+        System.out.println(a);
+
+        Object O = new Object();
     }
 }
