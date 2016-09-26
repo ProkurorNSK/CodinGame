c931c6dd42f396b0c1209a648fccd8fed7c018a3
ProkurorNSK <proka@ngs.ru>
2016-09-26 21:02:01 +0700

    to 15 lvl

---
+++ src/JavaBook/Box.java
@@ -0,0 +1,34 @@
+package JavaBook;
+
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Box implements Serializable{
+    private int width;
+    private int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        myBox.setHeight(20);
+        myBox.setWidth(50);
+
+        try {
+            FileOutputStream fs = new FileOutputStream("foo.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(myBox);
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
