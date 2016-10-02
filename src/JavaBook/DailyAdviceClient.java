54ca63a1c3037bb8b0f7b9a51b7acdeba4326fd1
ProkurorNSK <proka@ngs.ru>
2016-10-03 01:55:48 +0700

    to 15 lvl

---
+++ src/JavaBook/DailyAdviceClient.java
@@ -0,0 +1,26 @@
+package JavaBook;
+
+import java.net.*;
+import java.io.*;
+
+public class DailyAdviceClient {
+    public static void main(String[] args) {
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
+    }
+
+    private void go() {
+        try {
+            Socket s = new Socket("127.0.0.1", 4242);
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            String advice = reader.readLine();
+            System.out.println("Сугодня ты должен:" + advice);
+
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/JavaBook/SimpleChatClientA.java
@@ -0,0 +1,56 @@
+package JavaBook;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+public class SimpleChatClientA {
+
+    private JTextField outgoing;
+    private PrintWriter writer;
+
+    public static void main(String[] args) {
+        new SimpleChatClientA().go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,500);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetworking() {
+        try {
+            Socket sock = new Socket("127.0.0.1", 5000);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private class SendButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+}
