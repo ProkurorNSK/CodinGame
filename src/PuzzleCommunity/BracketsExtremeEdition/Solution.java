30574578c46548c5d68b507d9fe81f3070c28953
ProkurorNSK <proka@ngs.ru>
2017-04-28 20:30:20 +0700

    CommunityPuzzle

---
+++ src/PuzzleCommunity/BracketsExtremeEdition/Solution.java
@@ -0,0 +1,25 @@
+package PuzzleCommunity.BracketsExtremeEdition;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String expression = in.next();
+
+        String result = findAndDeletePair(expression);
+
+        System.out.println("true/false");
+    }
+
+    static String findAndDeletePair(String inStr) {
+        return findAndDeletePair(inStr);
+    }
+}
