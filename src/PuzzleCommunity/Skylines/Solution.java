71af919d0bfde6c4f29b68b98a151145761f95f6
ProkurorNSK <proka@ngs.ru>
2017-05-04 03:38:20 +0700

    Coders

---
+++ src/PuzzleCommunity/Skylines/Solution.java
@@ -0,0 +1,27 @@
+package PuzzleCommunity.Skylines;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int h = in.nextInt();
+            int x1 = in.nextInt();
+            int x2 = in.nextInt();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("linecount");
+    }
+}
